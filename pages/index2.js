681c0f6974c91a2e0d1b6854a6970ed9717fa0ed
JJ-quadour <96658466+JJ-quadour@users.noreply.github.com>
2023-05-25 20:59:36 +0300

    Update index2.js

--- pages/index2.js
+++ pages/index2.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import styles from 'pages/styles.module.css';
+import styles from './styles.module.css';
 
 export default function Home() {
   const name = 'Jawdat Kadour';
