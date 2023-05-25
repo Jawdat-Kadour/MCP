1f42248dbf4a6c3118411f9fa8c45dc518aab493
JJ-quadour <96658466+JJ-quadour@users.noreply.github.com>
2023-05-25 22:59:11 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '@styles/globals.css';
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+Application.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default Application;
