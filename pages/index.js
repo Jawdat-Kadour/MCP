9a48904c8f89900e3063495e9fa53d204b60b858
JJ-quadour <96658466+JJ-quadour@users.noreply.github.com>
2023-05-25 20:39:49 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,37 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  const name = 'Jawdat Kadour';
+  const age = 22;
+  const education = '4th year student at ITE';
+  const experience = 'Over a year of experience in Flutter/Dart development';
+  const skills = 'Experienced in Third party libraries and API\'s, state management, Flutter flavor, and app architecture';
+  const designSkills = 'Good designer';
+  const previousExperience = 'Previous experience in Mobile app development';
+  const agileDevelopment = 'Understanding of the agile development life cycle';
+  const communicationSkills = 'Strong communication and consultative skills';
+  const initiative = 'Self-initiated and proactive';
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Y-TECH - CV</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title={`Welcome to ${name}'s CV!`} />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Name: {name} <br />
+          Age: {age} <br />
+          Education: {education} <br />
+          Experience: {experience} <br />
+          Skills: {skills} <br />
+          Design Skills: {designSkills} <br />
+          Previous Experience: {previousExperience} <br />
+          Agile Development: {agileDevelopment} <br />
+          Communication Skills: {communicationSkills} <br />
+          Initiative: {initiative} <br />
         </p>
       </main>
 
