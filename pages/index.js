9a207ade2794f1937b48fd284cb13594a73ba0f0
JJ-quadour <96658466+JJ-quadour@users.noreply.github.com>
2023-05-26 14:58:46 +0000

    Pending changes exported from your codespace

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,12 @@ import Footer from '@components/Footer';
 import styles from './styles.module.css';
 
 const images = [
-  'public/IMG_20230525_222546_813.jpg', // Update the path to your personal photo
-  'image2.jpg', // Add other image file names here
-  'image3.jpg',
-  'image4.jpg',
+  '/Jood.jpg', // Update the path to your personal photo
+   // Add other image file names here
 ];
 
 export default function Home() {
-  const name = 'Jawdat Kadour';
+  const name = 'JAWDAT KADOUR';
   const age = 22;
   const education =
     '4th year student at ITE (Information Technology Engineering), specialized in Artificial Intelligence';
@@ -25,6 +23,8 @@ export default function Home() {
   const agileDevelopment = 'Understanding of the agile development life cycle';
   const communicationSkills = 'Strong communication and consultative skills';
   const initiative = 'Self-initiated and proactive';
+  const liveAt = 'Damascus';
+  const references = 'Available upon request';
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
@@ -32,29 +32,43 @@ export default function Home() {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
+  const email = 'jkad7619@gmail.com';
+  const whatsapp = '0991345578';
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Y-TECH - CV</title>
+        <title>CV - {name}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+  
       <main>
-        <Header title={`Welcome to ${name}'s CV!`} />
+        <Header title={`Welcome to ${name}'s CV`} />
         <div className={styles.cv}>
-          <h1 className={styles.name}>{name}</h1>
-          <img className={styles.photo} src={images[currentImageIndex]} alt="Personal Photo" />
           <div className={styles.details}>
-            <div>
+            <div className={styles.leftSection}>
+              <div className={styles.profile}>
+                <img className={styles.photo} src={images[currentImageIndex]} alt="Personal Photo" />
+                <h1 className={styles.name}>{name}</h1>
+                <h2 className={styles.sectionTitle}>EDUCATION</h2>
+              <p>Bachelor of IT Engineering.</p>
+              <p>University of Damascus | 2019-present</p>
+              </div>
+              <div className={styles.additionalInfo}>
+                <h2 className={styles.sectionTitle}>Additional Information</h2>
+                <p>Live at {liveAt}</p>
+                <p>References: {references}</p>
+              </div>
+              <div className={styles.contact}>
+                <h2 className={styles.sectionTitle}>Contact Me</h2>
+                <p>Email: {email}</p>
+                <p>WhatsApp: {whatsapp}</p>
+              </div>
+            </div>
+            <div className={styles.rightSection}>
               <h2 className={styles.sectionTitle}>Personal Information</h2>
               <p>Age: {age}</p>
               <p>Education: {education}</p>
-              <div className={styles.progressContainer}>
-                <p>English Speaking:</p>
-                <progress className={styles.progress} value="80" max="100"></progress>
-              </div>
-            </div>
-            <div>
               <h2 className={styles.sectionTitle}>Professional Summary</h2>
               <p>{experience}</p>
               <p>{skills}</p>
@@ -69,18 +83,23 @@ export default function Home() {
               <p>{previousExperience}</p>
               <p>{agileDevelopment}</p>
               <p>{initiative}</p>
+              <h2 className={styles.sectionTitle}>Programming Languages</h2>
+              <ul className={styles.projectsList}>
+                <li>Java</li>
+                <li>Dart</li>
+                <li>HTML/Css/JS</li>
+                <li>C++</li>
+              </ul>
+              
             </div>
           </div>
         </div>
-        <button className={styles.button} onClick={handleNextImage}>
-          Next Image
-        </button>
-        <Link href="/works">
-          <a className={styles.button}>View My Works</a>
+        <Link href="/myWork">
+          <a className={styles.button}>Some Of My Works</a>
         </Link>
       </main>
-
+  
       <Footer />
     </div>
   );
-}
+  }  
