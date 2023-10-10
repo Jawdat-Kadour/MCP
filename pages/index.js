c7236266d397ce4c4c71c0f374454688b9d1b34f
JJ-quadour <jkad7619@gmail.com>
2023-10-10 20:17:29 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,198 @@
+// import Head from "next/head";
+// import { useState } from "react";
+// import Link from "next/link";
+// import Header from "@components/Header";
+// import Footer from "@components/Footer";
+// import styles from "./styles.module.css";
+
+// const images = ["/Jood.jpg"]; // Update the path to your personal photo
+
+// const buttonDetails = {
+//   me: {
+//     title: "Me",
+//     content: [
+//       { label: "Age", value: 22 },
+//       {
+//         label: "Education",
+//         value:
+//           "5th year student at ITE (Information Technology Engineering), specialized in Artificial Intelligence",
+//       },
+//       { label: "Location", value: "Damascus" },
+//     ],
+//   },
+//   experiences: {
+//     title: "My Experiences",
+//     content: [
+//       {
+//         label: "Professional Summary",
+//         value:
+//           "I’m a Python developer and AI enthusiast. And I have over a year of experience in Flutter/Dart development",
+//       },
+//       {
+//         label: "Skills",
+//         value:
+//           "Experienced in Third-party libraries and Restful API's, GetX, Push Notification, Google Maps, State Management, Flutter flavor, And app architecture",
+//       },
+//       {
+//         label: "Previous Experience",
+//         value: "Previous experience in Mobile-App/Web development",
+//       },
+//       {
+//         label: "Agile Development",
+//         value: "Understanding of the agile development life cycle",
+//       },
+//       { label: "Initiative", value: "Self-initiated and proactive" },
+//       { label: "Design Skills", value: "Good designer" },
+//       { label: "English Speaking", value: "Fluent" },
+//       {
+//         label: "Communication Skills",
+//         value: "Strong communication and consultative skills",
+//       },
+//       {
+//         label: "Programming Languages",
+//         value: ["Python", "Java", "Dart", "HTML/CSS/JS", "C++"],
+//       },
+//     ],
+//   },
+//   projects: {
+//     title: "Projects",
+//     projectsList: [
+//       {
+//         title: "Yugo",
+//         description:
+//           "Mobile application designed to connect passengers with nearby taxi drivers.",
+//         role: "Developing GUIs included with Google Maps, Registration GUIs",
+//       },
+//       {
+//         title: "Neura",
+//         description:
+//           "Chatbot application that provides guidance to users for maintaining their health and well-being.",
+//         role: "Working on the modules and All the Front-End developing",
+//       },
+//     ],
+//   },
+//   contact: {
+//     title: "Contact Me",
+//     content: [
+//       { label: "Email", value: "jkad7619@gmail.com" },
+//       { label: "WhatsApp", value: "0991345578" },
+//       { label: "References", value: "Available upon request" },
+//     ],
+//   },
+// };
+
+// export default function Home() {
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+//   const [activeSection, setActiveSection] = useState(null);
+//   const [currentProject, setCurrentProject] = useState(null);
+
+//   const handleButtonClick = (section) => {
+//     setActiveSection(activeSection === section ? null : section);
+//     setCurrentProject(null); // Reset currentProject when switching sections
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Professional Portfolio - JAWDAT KADOUR</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main>
+//         <Header title="Welcome to My Professional Portfolio: " />
+//         <div className={styles.cv}>
+//           <div className={styles.profile}>
+//             <img
+//               className={styles.photo}
+//               src={images[currentImageIndex]}
+//               alt="Personal Photo"
+//             />
+//             <h1 className={styles.name}>JAWDAT KADOUR</h1>
+//             <div className={styles.buttonGroup}>
+//               {Object.keys(buttonDetails).map((section) => (
+//                 <button
+//                   key={section}
+//                   className={`${styles.button} ${
+//                     activeSection === section ? styles.active : ""
+//                   }`}
+//                   onClick={() => handleButtonClick(section)}
+//                 >
+//                   {buttonDetails[section].title}
+//                 </button>
+//               ))}
+//             </div>
+//           </div>
+
+//           <div className={styles.details}>
+//             {activeSection && activeSection !== "projects" && (
+//               <div>
+//                 <h2 className={styles.sectionTitle}>
+//                   {buttonDetails[activeSection].title}
+//                 </h2>
+//                 {buttonDetails[activeSection].content.map((item) => (
+//                   <p key={item.label}>
+//                     {item.label}:{" "}
+//                     {Array.isArray(item.value)
+//                       ? item.value.join(", ")
+//                       : item.value}
+//                   </p>
+//                 ))}
+//               </div>
+//             )}
+
+//             {activeSection === "projects" && (
+//               <div>
+//                 <h2 className={styles.sectionTitle}>Projects:</h2>
+//                 <ul className={styles.projectsList}>
+//                   {activeSection === "projects" && (
+//                     <div>
+//                       {/* <h2 className={styles.sectionTitle}>Projects:</h2> */}
+//                       <ul className={styles.projectsList}>
+//                         {buttonDetails[activeSection].projectsList.map(
+//                           (project, index) => (
+//                             <Link
+//                               key={index}
+//                               href={
+//                                 project.title.toLowerCase() === "yugo"
+//                                   ? "/yugo"
+//                                   : "/neura"
+//                               }
+//                             >
+//                               <a>
+//                                 <div>
+//                                   <button
+//                                     className={`${styles.button} ${
+//                                       currentProject === index
+//                                         ? styles.active
+//                                         : ""
+//                                     }`}
+//                                     onClick={() => setCurrentProject(index)}
+//                                   >
+//                                     {project.title}
+//                                   </button>
+//                                   {currentProject === index && <div></div>}
+//                                 </div>
+//                               </a>
+//                             </Link>
+//                           )
+//                         )}
+//                       </ul>
+//                     </div>
+//                   )}
+//                 </ul>
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       </main>
+
+//       <Footer />
+//     </div>
+//   );
+// }
 import Head from "next/head";
 import { useState } from "react";
+import Link from "next/link";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import styles from "./styles.module.css";
@@ -67,6 +260,21 @@ const buttonDetails = {
         description:
           "Chatbot application that provides guidance to users for maintaining their health and well-being.",
         role: "Working on the modules and All the Front-End developing",
+      },
+      {
+        title: "CelestialBodiesSimulator",
+        description: "Simulator for celestial bodies exploration.",
+        role: "Developing the simulation engine and UI",
+      },
+      {
+        title: "Filmatica",
+        description: "Application for managing and discovering movies.",
+        role: "Full-stack development",
+      },
+      {
+        title: "AngryBirds3D",
+        description: "3D version of the popular Angry Birds game.",
+        role: "Game development and design",
       },
     ],
   },
@@ -143,25 +351,49 @@ export default function Home() {
               <div>
                 <h2 className={styles.sectionTitle}>Projects:</h2>
                 <ul className={styles.projectsList}>
-                  {buttonDetails[activeSection].projectsList.map(
-                    (project, index) => (
-                      <div key={index}>
-                        <button
-                          className={`${styles.button} ${
-                            currentProject === index ? styles.active : ""
-                          }`}
-                          onClick={() => setCurrentProject(index)}
-                        >
-                          {project.title}
-                        </button>
-                        {currentProject === index && (
-                          <div>
-                            <p>Description: {project.description}</p>
-                            <p>Role: {project.role}</p>
-                          </div>
+                  {activeSection === "projects" && (
+                    <div>
+                      <ul className={styles.projectsList}>
+                        {buttonDetails[activeSection].projectsList.map(
+                          (project, index) => (
+                            <Link
+                              key={index}
+                              href={
+                                project.title.toLowerCase() === "yugo"
+                                  ? "/yugo"
+                                  : project.title.toLowerCase() === "neura"
+                                  ? "/neura"
+                                  : project.title.toLowerCase() ===
+                                    "celestialbodiessimulator"
+                                  ? "/galaxy"
+                                  : project.title.toLowerCase() === "filmatica"
+                                  ? "/movies"
+                                  : project.title.toLowerCase() ===
+                                    "angrybirds3d"
+                                  ? "/angryBirds"
+                                  : "#"
+                              }
+                            >
+                              <a>
+                                <div>
+                                  <button
+                                    className={`${styles.button} ${
+                                      currentProject === index
+                                        ? styles.active
+                                        : ""
+                                    }`}
+                                    onClick={() => setCurrentProject(index)}
+                                  >
+                                    {project.title}
+                                  </button>
+                                  {currentProject === index && <div></div>}
+                                </div>
+                              </a>
+                            </Link>
+                          )
                         )}
-                      </div>
-                    )
+                      </ul>
+                    </div>
                   )}
                 </ul>
               </div>
@@ -174,166 +406,3 @@ export default function Home() {
     </div>
   );
 }
-
-// import Head from "next/head";
-// import { useState } from "react";
-// import Header from "@components/Header";
-// import Footer from "@components/Footer";
-// import Link from "next/link"; // Import Link from Next.js
-// import styles from "./styles.module.css";
-
-// const images = ["/Jood.jpg"]; // Update the path to your personal photo
-
-// const buttonDetails = {
-//   me: {
-//     title: "Me",
-//     content: [
-//       { label: "Age", value: 22 },
-//       {
-//         label: "Education",
-//         value:
-//           "5th year student at ITE (Information Technology Engineering), specialized in Artificial Intelligence",
-//       },
-//       { label: "Location", value: "Damascus" },
-//     ],
-//   },
-//   experiences: {
-//     title: "My Experiences",
-//     content: [
-//       {
-//         label: "Professional Summary",
-//         value:
-//           "I’m a Python developer and AI enthusiast. And I have over a year of experience in Flutter/Dart development",
-//       },
-//       {
-//         label: "Skills",
-//         value:
-//           "Experienced in Third-party libraries and Restful API's, GetX, Push Notification, Google Maps, State Management, Flutter flavor, And app architecture",
-//       },
-//       {
-//         label: "Previous Experience",
-//         value: "Previous experience in Mobile-App/Web development",
-//       },
-//       {
-//         label: "Agile Development",
-//         value: "Understanding of the agile development life cycle",
-//       },
-//       { label: "Initiative", value: "Self-initiated and proactive" },
-//       { label: "Design Skills", value: "Good designer" },
-//       { label: "English Speaking", value: "Fluent" },
-//       {
-//         label: "Communication Skills",
-//         value: "Strong communication and consultative skills",
-//       },
-//       {
-//         label: "Programming Languages",
-//         value: ["Python", "Java", "Dart", "HTML/CSS/JS", "C++"],
-//       },
-//     ],
-//   },
-// };
-
-// export default function Home() {
-//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-//   const [activeSection, setActiveSection] = useState(null);
-//   const [currentProject, setCurrentProject] = useState(null);
-
-//   const handleButtonClick = (section) => {
-//     if (section === "projects") {
-//       // Navigate to the "myWorks" page when clicking on the "Projects" button
-//       // Use Next.js Link component to navigate
-//       window.location.href = "/myWorks";
-//     } else {
-//       setActiveSection(activeSection === section ? null : section);
-//       setCurrentProject(null); // Reset currentProject when switching sections
-//     }
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>Professional Portfolio - JAWDAT KADOUR</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main>
-//         <Header title="Welcome to My Professional Portfolio: " />
-//         <div className={styles.cv}>
-//           <div className={styles.profile}>
-//             <img
-//               className={styles.photo}
-//               src={images[currentImageIndex]}
-//               alt="Personal Photo"
-//             />
-//             <h1 className={styles.name}>JAWDAT KADOUR</h1>
-//             <div className={styles.buttonGroup}>
-//               {Object.keys(buttonDetails).map((section) => (
-//                 <button
-//                   key={section}
-//                   className={`${styles.button} ${
-//                     activeSection === section ? styles.active : ""
-//                   }`}
-//                   onClick={() => handleButtonClick(section)}
-//                 >
-//                   {buttonDetails[section].title}
-//                 </button>
-//               ))}
-//             </div>
-//           </div>
-
-//           <div className={styles.details}>
-//             {activeSection && activeSection !== "projects" && (
-//               <div>
-//                 <h2 className={styles.sectionTitle}>
-//                   {buttonDetails[activeSection].title}
-//                 </h2>
-//                 {buttonDetails[activeSection].content.map((item) => (
-//                   <p key={item.label}>
-//                     {item.label}:{" "}
-//                     {Array.isArray(item.value)
-//                       ? item.value.join(", ")
-//                       : item.value}
-//                   </p>
-//                 ))}
-//               </div>
-//             )}
-
-//             {activeSection === "projects" && (
-//               <div>
-//                 <h2 className={styles.sectionTitle}>Projects:</h2>
-//                 <ul className={styles.projectsList}>
-//                   {buttonDetails[activeSection].projectsList.map(
-//                     (project, index) => (
-//                       <div key={index}>
-//                         <Link href="/myWorks">
-//                           {" "}
-//                           {/* Use Link component */}
-//                           <a
-//                             className={`${styles.button} ${
-//                               currentProject === index ? styles.active : ""
-//                             }`}
-//                             onClick={() => setCurrentProject(index)}
-//                           >
-//                             {project.title}
-//                           </a>
-//                         </Link>
-//                         {currentProject === index && (
-//                           <div>
-//                             <p>Description: {project.description}</p>
-//                             <p>Role: {project.role}</p>
-//                           </div>
-//                         )}
-//                       </div>
-//                     )
-//                   )}
-//                 </ul>
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       </main>
-
-//       <Footer />
-//     </div>
-//   );
-// }
---
+++ pages/yugo.js
@@ -0,0 +1,243 @@
+import Head from "next/head";
+import { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./works.module.css";
+
+export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slides = [
+    {
+      id: 1,
+      image: "/6.png",
+      alt: "Image 6",
+      legend:
+        "In this project, I created an AI healthcare chat bot that can diagnose diseases, suggest treatments, provide information about medicines and food systems, and perform menstrual and water analysis. I used Python as the main programming language, along with various libraries and frameworks such as TensorFlow, NLTK",
+    },
+    {
+      id: 6,
+      image: "/1.png",
+      alt: "Image 1",
+      legend:
+        "chatbot application provides guidance to users for maintaining their health and well-being",
+    },
+    {
+      id: 2,
+      image: "/2.png",
+      alt: "Image 2",
+      legend:
+        "As part of my work on Neura, I have developed the Neural networks and the GUIs",
+    },
+    {
+      id: 3,
+      image: "/3.png",
+      alt: "Image 3",
+      legend:
+        "I have implemented most of the services of the chatbot in addition of implementing the user interface elements such as buttons, text fields, and menus...",
+    },
+    {
+      id: 4,
+      image: "/4.png",
+      alt: "Image 4",
+      legend:
+        "Additionally, I have integrated all the user interface elements with the underlying logic of the application",
+    },
+    {
+      id: 5,
+      image: "/5.png",
+      alt: "Image 5",
+      legend:
+        "allowing users to interact with the chatbot and receive guidance on their health",
+    },
+    {
+      id: 7,
+      image: "/7.png",
+      alt: "Image 7",
+      legend: "SERVICES PROVIDED BY NEURA :) ",
+    },
+    {
+      id: 8,
+      image: "/8.png",
+      alt: "Image 8",
+      legend:
+        "Medication reminder: Neura can help you stay on track with your medication schedule by sending you timely reminders.",
+    },
+    {
+      id: 9,
+      image: "/conv.png",
+      alt: "Image 9",
+      legend:
+        "Neura can handle all possible messages from users due to his immense database of intents and patterns.",
+    },
+    {
+      id: 10,
+      image: "/info.png",
+      alt: "Image 10",
+      legend:
+        "Presentation of disease-specific information: Neura offers insights on specific diseases, including symptoms, causes, prevention, and management.",
+    },
+    {
+      id: 11,
+      image: "/article.png",
+      alt: "Image 11",
+      legend:
+        "it also provides scientific articles from the FDA and PubMed database.",
+    },
+    {
+      id: 12,
+      image: "/treat.png",
+      alt: "Image 12",
+      legend:
+        "Treatments for diseases: Neura provides information on plant-based medicinal treatments and holistic approaches for various diseases.",
+    },
+    {
+      id: 13,
+      image: "/warn.png",
+      alt: "Image 13",
+      legend: "it proviedes also warnings of using these treatmetns",
+    },
+    {
+      id: 14,
+      image: "/system.png",
+      alt: "Image 14",
+      legend:
+        "Food systems: Neura offers information on healthy eating, including meal planning, nutrient-rich foods, and recipes..",
+    },
+    {
+      id: 15,
+      image: "/period.png",
+      alt: "Image 15",
+      legend:
+        "Period tracker: Neura helps you track and monitor your menstrual cycle.",
+    },
+    {
+      id: 16,
+      image: "/med.png",
+      alt: "Image 16",
+      legend:
+        "Presentation of medicine-specific information: Neura can provide specific details about medications, including usage instructions and potential side effects. ",
+    },
+    {
+      id: 17,
+      image: "/serv.png",
+      alt: "Image 17",
+      legend:
+        "Weight tracker: Neura helps you track your weight and provides guidance on maintaining a healthy weight.",
+    },
+    {
+      id: 18,
+      image: "/serv2.png",
+      alt: "Image 18",
+      legend:
+        "Mood tracker: Neura assists in tracking and managing your mood for emotional well-being.",
+    },
+    {
+      id: 19,
+      image: "/serv3.png",
+      alt: "Image 19",
+      legend:
+        " Fitness tips: Neura provides suggestions for physical activities and exercises to support your fitness goals.",
+    },
+    {
+      id: 20,
+      image: "/joke.png",
+      alt: "Image 20",
+      legend:
+        "Mental well-being support: Neura provides support and resources for improving mental well-being and managing stress. Personalized health insights: Neura analyzes your health data to provide personalized insights and recommendations.",
+    },
+    {
+      id: 21,
+      image: "/water.png",
+      alt: "Image 21",
+      legend:
+        "Analyzing the percentage of water in the body and knowing the appropriate amount that he likes to eat on a daily basis.",
+    },
+    {
+      id: 22,
+      image: "/menu.png",
+      alt: "Image 22",
+      legend:
+        "General health guidance: Neura provides personalized health advice and tips for overall well-being. Nutritional advices: Neura offers guidance on nutrition, including healthy eating habits and food choices.",
+    },
+    {
+      id: 23,
+      image: "/bye.png",
+      alt: "Image 23",
+      legend:
+        "Providing a diet that suits many desires of the user and providing a diet based on personal specifications such as weight, height and other specifications...",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (current, next) => setCurrentSlide(next),
+  };
+
+  return (
+    <>
+      <Head>
+        <title>YUGO</title>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>YUGO</h1>
+
+        <div className={styles.projectsContainer}>
+          {/* <h2 className={styles.sectionTitle}>Projects:</h2> */}
+          <ol className={styles.projectsList}>
+            <li className={styles.projectItem}>
+              <span className={styles.projectTitle}>Yugo</span> | Flutter
+              Developer | March 2022 - June 2022
+              <ul className={styles.projectDetails}>
+                <li>
+                  Description: Mobile application designed to connect passengers
+                  with nearby taxi drivers.
+                </li>
+                <li>
+                  Role: Developing GUIs included with Google Maps, Registration
+                  GUIs
+                </li>
+              </ul>
+            </li>
+            {/* <li className={styles.projectItem}>
+              <span className={styles.projectTitle}>Neura</span> |
+              Flutter-Python Developer | March 2023 - July 2023
+              <ul className={styles.projectDetails}>
+                <li>
+                  Description: Chatbot application that provides guidance to
+                  users for maintaining their health and well-being.
+                </li>
+                <li>
+                  Role: Working on the modules and All the Front-End developing.
+                </li>
+              </ul>
+            </li> */}
+          </ol>
+        </div>
+
+        <div className={styles.carouselContainer}>
+          <Slider {...settings}>
+            {slides.map((slide) => (
+              <div key={slide.id} className={styles.slide}>
+                <img
+                  className={styles.image}
+                  src={slide.image}
+                  alt={slide.alt}
+                />
+                <div className={styles.description}>
+                  <p className={styles.legend}>{slide.legend}</p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
